8b1834b440d85cbb9f1e61423df40f4e7c95ba88
knickoriuk <nickoriuk@live.com>
2022-03-18 01:28:15 -0400

    General Tidying, Calling Convention
    - Main loop uses proper registers for appropriate calling convention
    - one new function `key_pressed()` to replace `pressed_a()`, `pressed_d()`, etc.
    - main loop cleaned up
    - Clam and piranha stamp functions condensed into one instead of pairs, will select correct sprite based on global data
    - `get_bg_color()` now works
    - `unstamp_crab()` and `generate_background()` both call on `get_bg_color()` now
    - modified some function header notation

--- game.asm
+++ game.asm
@@ -67,19 +67,33 @@ clam:		.space		12
 
 .text
 .globl main
+
+########## Register Assignment for main() ##########
+# $s0 - `world` data pointer	# todo: if this stays the same, do we need to load it in 
+				# functions? what is allowed? do they allow things? let's 
+				# find out.
+# $s1 - `crab` data pointer
+# $s2 -
+# $s3 -
+# $s4 -
+# $s5 -
+# $s6 -
+# $s7 -
+# $t0 - temporary values
+
 ########## Initialize Game Values ##########
 main:	# `world` initial data
-	la $t0, world
-	li $t1, 4
-	sw $t1, ($t0)		# world.level = 4
+	la $s0, world
+	li $t0, 4
+	sw $t0, ($s0)		# world.level = 4
 	
 	# `crab` initial data
-	la $t0, crab
-	li $t1, INIT_POS
-	add $t1, $t1, $gp
-	sw $t1, ($t0)		# crab.pos = addr($gp) + INIT_POS
-	li $t1, 0
-	sw $t1, 4($t0)		# crab.state = 0
+	la $s1, crab
+	li $t0, INIT_POS
+	add $t0, $t0, $gp
+	sw $t0, ($s1)		# crab.pos = addr($gp) + INIT_POS
+	li $t0, 0
+	sw $t1, 4($s1)		# crab.state = 0
 	
 	# Fill in background
 	jal generate_background
@@ -87,45 +101,19 @@ main:	# `world` initial data
 	
 ########## Get Keyboard Input ##########
 
-detect_keystroke:
-	# KEYSTROKE
-	#	+0: 1 if key hit, 0 if key not hit
-	#	+4: ascii value of key that was hit
-
-	li $t9, KEYSTROKE  	# $t9 
-	lw $t8, 0($t9) 		
-	bne $t8, 1, no_key_hit	# if no key was hit, branch to `no_key hit` 
+main_loop:
+	li $t0, KEYSTROKE
+	lw $t0, 0($t0) 			# $t0 = 1 if key hit, 0 otherwise
+	bne $t0, 1, update_display	# if no key was hit, branch to `update_display` 
+				
 				# If it reaches here, a key was hit.
-	lw $t9, 4($t9) 		# $t9 = last key hit 
-	beq $t9, 0x61, key_a  	# If $t9 == 'a', branch to `key_a`
-	beq $t9, 0x64, key_d  	# If $t9 == 'd', branch to `key_d`
-	beq $t9, 0x77, key_w  	# If $t9 == 'w', branch to `key_w`
-	beq $t9, 0x70, key_p  	# If $t9 == 'p', branch to `key_p`
-	j no_key_hit		# Otherwise, treat like no key pressed
-	
+	jal unstamp_crab	# remove crab before we modify its location
+	jal key_pressed		# Update position in `crab` struct	
+
 ########## Update Display ##########
 
-key_a:	jal unstamp_crab
-	jal pressed_a		# Move character left
-	j key_input_done
-
-key_d:	jal unstamp_crab
-	jal pressed_d		# Move character right
-	j key_input_done
-
-key_w:	jal unstamp_crab
-	jal pressed_w		# Jump character
-	j key_input_done
-
-key_p:	jal unstamp_crab #?
-	jal pressed_p		# Reset Game
-	j key_input_done
-
-key_input_done:
+update_display:
 	jal stamp_crab		# Add crab to display
-
-no_key_hit:
-	# Don't need to update character position
 	
 ########## Sleep and Repeat ##########
 
@@ -134,12 +122,10 @@ no_key_hit:
 	li $v0, 32
 	syscall
 	
-	j detect_keystroke	# Check for next key
+	j main_loop	# Jump back to main loop, checking for next key press
 
 ########## Testing Functions, For Now #########
-	
-	
-	
+		
 	# Seahorse
 	addi $sp, $sp, -4	# make room on stack
 	li $t2, 7000		# $t2 = 7000
@@ -147,19 +133,12 @@ no_key_hit:
 	sw $t2, 0($sp)		# push to stack
 	jal stamp_seahorse
 	
-	# Closed Clam
+	# Clam
 	addi $sp, $sp, -4	
 	li $t2, 32600		
 	add $t2, $t2, $gp	
 	sw $t2, 0($sp)		
-	jal stamp_closed_clam
-	
-	# Open Clam
-	addi $sp, $sp, -4
-	li $t2, 26000		
-	add $t2, $t2, $gp	
-	sw $t2, 0($sp)		
-	jal stamp_open_clam
+	jal stamp_clam
 	
 	# Pufferfish
 	addi $sp, $sp, -4
@@ -176,90 +155,67 @@ exit:	li  $v0, 10
 #	KEYBOARD INPUT FUNCTIONS					#
 #########################################################################
 
-# pressed_a():
-#	Move crab left if applicable, update new location in `crab` struct
-#	Additionally, toggle walk state between 0 and 1
-#	TODO: Check for walls
-pressed_a:
-	la $t1, crab
+# key_pressed():
+#	Determines and updates new position for the crab given the current key pressed.
+#	$t1: addr(crab), $t2: crab_pos, $t3: crab_state, $t9: key_input
+key_pressed:
+	li $t9, KEYSTROKE  	# $t9 
+	lw $t9, 4($t9) 		# $t9 = last key hit 
+	
+	# Get crab data
+	la $t1, crab	# $t1 = addr(crab)
 	lw $t2, 0($t1)	# $t2 = crab.position
 	lw $t3, 4($t1)	# $t3 = crab.state
 	
-	beq $t3, 2, pa_move_left	# If currently jumping (state 2), just move left
-	beq $t3, 3, pa_exit		# If dead (state 3), don't change anything
-	beq $t3, 0, pa_toggle_1		# If state==0, toggle walk state from 0 -> 1
+	# If dead (state 3), don't change anything.
+	beq $t3, 3, key_input_done
 	
-	# Otherwise toggle walk state from 1 -> 0
-	li $t3, 0		
-	sw $t3, 4($t1)		# crab.state = 0
-	j pa_move_left
+	# Check which key pressed
+	beq $t9, 0x61, key_a  	# If $t9 == 'a', branch to `key_a`
+	beq $t9, 0x64, key_d  	# If $t9 == 'd', branch to `key_d`
+	beq $t9, 0x77, key_w  	# If $t9 == 'w', branch to `key_w`
+	beq $t9, 0x70, key_p  	# If $t9 == 'p', branch to `key_p`
+	j key_input_done	# Otherwise, treat like no key pressed
 
-pa_toggle_1:
-	li $t3, 1
-	sw $t3, 4($t1)		# crab.state = 1
-	
-pa_move_left:			# Update position stored in `crab`
+key_a:	# MOVE LEFT		# Update position stored in `crab`
 	addi $t2, $t2, -4	# $t2 = crab.position - 4
 	sw $t2, 0($t1)		# crab.position = crab.position - 4
-pa_exit:
-	jr $ra
-# ---------------------------------------------------------------------------------------
-	
-# pressed_d():
-#	Move crab right if applicable, update new location in `crab` struct
-#	Additionally, toggle walk state between 0 and 1
-#	TODO: Check for walls
-pressed_d:
-	la $t1, crab
-	lw $t2, 0($t1)	# $t2 = crab.position
-	lw $t3, 4($t1)	# $t3 = crab.state
-	
-	beq $t3, 2, pd_move_right	# If currently jumping (state 2), just move right
-	beq $t3, 3, pd_exit		# If dead (state 3), don't change anything
-	beq $t3, 0, pd_toggle_1		# If state==0, toggle walk state from 0 -> 1
-	
-	# Otherwise toggle walk state from 1 -> 0
-	li $t3, 0		
-	sw $t3, 4($t1)		# crab.state = 0
-	j pd_move_right
 
-pd_toggle_1:
-	li $t3, 1
-	sw $t3, 4($t1)		# crab.state = 1
-	
-pd_move_right:
+	bge $t3, 2, key_input_done	# If currently jumping (state 2), don't toggle walk state
+	j key_toggle_check		# Toggle between walk states (state 1 and 0)
+
+key_d:	# MOVE RIGHT		# Update position stored in `crab`
 	addi $t2, $t2, 4	# $t2 = crab.position + 4
 	sw $t2, 0($t1)		# crab.position = crab.position + 4
-pd_exit:
-	jr $ra
-# ---------------------------------------------------------------------------------------
-	
-# pressed_w():
-#	Jump crab if applicable
-#	Update new position in `crab` struct
-#	TODO: Wall detection, gravity, falling, etc.
-pressed_w:
-	la $t1, crab
-	lw $t2, 0($t1)	# $t2 = crab.position
-	lw $t3, 4($t1)	# $t3 = crab.state
-	
-	beq $t3, 2, pw_move_up	# If currently jumping (state 2), just move up
-	beq $t3, 3, pw_exit	# If dead (state 3), don't change anything
-	
-	# Ensure state is set to 2 (jumping)
-	li $t3, 2		
-	sw $t3, 4($t1)		# crab.state = 2
-	
-pw_move_up:
-	addi $t2, $t2, -WIDTH	# $t2 = crab.position - WIDTH
-	sw $t2, 0($t1)		# crab.position = crab.position + 1
-pw_exit:
-	jr $ra
-# ---------------------------------------------------------------------------------------
 
-# pressed_p():
-#	Reset game. TBD
-pressed_p:
+	bge $t3, 2, key_input_done	# If currently jumping (state 2), don't toggle walk state
+	j key_toggle_check		# Toggle between walk states (state 1 and 0)
+
+key_w:	# JUMP			# Update position stored in `crab`
+	addi $t2, $t2, -WIDTH	# $t2 = crab.position - WIDTH
+	sw $t2, 0($t1)		# crab.position = crab.position - WIDTH
+	li $t3, 2		# $t3 = 2
+	sw $t3, 4($t1)		# crab.state = 2
+	j key_input_done
+
+key_p:	# RESET
+	# TODO
+	j key_input_done
+	
+key_toggle_check:
+	beq $t3, 0, key_toggle_1	# If state==0, toggle walk state from 0 -> 1
+	
+	# Toggle crab.state from 1 to 0
+	li $t3, 0
+	sw $t3, 4($t1)		# crab.state = 0
+	j key_input_done
+	
+key_toggle_1:
+	# Toggle crab.state from 0 to 1
+	li $t3, 1
+	sw $t3, 4($t1)		# crab.state = 1
+
+key_input_done:
 	jr $ra
 # ---------------------------------------------------------------------------------------
 
@@ -270,34 +226,26 @@ pressed_p:
 
 # generate_background():
 #	Fills the display with a background colour
-# 	Uses registers $t0, $t1, $t2, $t9
-generate_background:
+# 	$t0: index, $t1: max_index, $t2: pixel_address, $t9: bg_color
+generate_background:	
+	# Push return address onto stack
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	# Obtain background color based on level in `world` struct
+	jal get_bg_color
+	
+	# Pop result and old return address from stack
+	lw $t9, 0($sp)		# $t9 = sea colour
+	lw $ra, 4($sp)		# $ra = old return address
+	addi $sp, $sp, 8
+	
+	# Set up indices of iteration
 	li $t0, 0		# $t0 = i
 	li $t1, 32768		# $t1 = 32768 (128*64*4)
-	
-	# Determine bg color based on level in `world` struct
-	la $t3, world
-	lw $t3, ($t3)		# $t3 = world.level
-	beq $t3, 0, bg_level_0	# branch if world.level == 0
-	beq $t3, 1, bg_level_1	# branch if world.level == 1
-	beq $t3, 2, bg_level_2	# branch if world.level == 2
-	beq $t3, 3, bg_level_3	# branch if world.level == 3
-	li $t9, SEA_COL_4	# $t8 = bg color 4
-	j bg_loop
-bg_level_0:
-	li $t9, SEA_COL_0	# $t8 = bg color 0
-	j bg_loop
-bg_level_1:
-	li $t9, SEA_COL_1	# $t8 = bg color 1
-	j bg_loop
-bg_level_2:
-	li $t9, SEA_COL_2	# $t8 = bg color 2
-	j bg_loop
-bg_level_3:
-	li $t9, SEA_COL_3	# $t8 = bg color 3
-	
+
 bg_loop:
-	beq $t0, $t1, bg_end	# branch to `bg_end` if i = 8192
+	beq $t0, $t1, bg_end	# branch to `bg_end` if i == 32768
 	
 	# Colour the ith pixel
 	add $t2, $gp, $t0	# $t2 = addr($gp) + i
@@ -320,7 +268,7 @@ build_platform:
 
 # stamp_crab():
 # 	"Stamps" the crab onto the display at crab.position
-#	Uses registers $t0, $t1, $t2, $t3, $t4
+#	$t0: pixel_address, $t1-$t4: colours, $t5: world, $t6: temp
 stamp_crab:
 	li $t1, 0x00cc552d	# $t1 = crab base
 	li $t2, 0x00a33615	# $t2 = crab shell
@@ -424,10 +372,10 @@ stamp_crab:
 # ---------------------------------------------------------------------------------------
 
 
-# stamp_open_clam(*pixel):
-# 	"Stamps" a open clam shell onto the display given it is positioned at *pixel
-#	Uses registers $t0-$t7
-stamp_open_clam:
+# stamp_clam(*pixel):
+# 	"Stamps" a clam shell onto the display given it is positioned at *pixel
+#	$t0: pixel_address, $t1-$t5: colors, $t6: world, $t7: temp
+stamp_clam:
 	li $t1, 0x00c496ff	# $t1 = shell midtone
 	li $t2, 0x00c7a3f7	# $t2 = shell highlight
 	li $t3, 0x009a7ac7	# $t3 = shell lo-light
@@ -449,7 +397,13 @@ stamp_open_clam:
 	lw $t0, 0($sp)		# $t0 = address of pixel
 	addi, $sp, $sp, 4	# reclaim space on stack
 	
-	# Color the pixels appropriately
+	# Determine if clam is open or closed
+	la $t7, clam
+	lw $t7 8($t7)		# $t7 = clam.state
+	beq $t7, 0, sc_closed	# if clam.state == 0, branch to `sc_closed`
+				# else, clam is open
+	
+	# Stamp an OPEN clam
 	sw $t1, -12($t0)
 	sw $t1, -8($t0)
 	sw $t1, -4($t0)
@@ -563,36 +517,10 @@ stamp_open_clam:
 	sw $t1, 4($t0)
 	sw $t1, 12($t0)
 	sw $t1, 16($t0)
+	j sc_exit
 	
-	# Return to caller
-	jr $ra
-# ---------------------------------------------------------------------------------------
-
-
-# stamp_closed_clam(*pixel):
-# 	"Stamps" a closed clam shell onto the display given it is positioned at *pixel
-#	Uses registers $t0-$t3, $t6, $t7
-stamp_closed_clam:
-	li $t1, 0x00c496ff	# $t1 = shell midtone
-	li $t2, 0x00c7a3f7	# $t2 = shell highlight
-	li $t3, 0x009a7ac7	# $t3 = shell lo-light
-	
-	# Determine darkening factor
-	la $t6, world
-	lw $t6, ($t6)		# $t6 = world.level
-	li $t7, DARKNESS	# 
-	mul $t7, $t7, $t6	# $t7 = $t7 * world.level
-	
-	# Darken colors based on darkening factor
-	sub $t1, $t1, $t7
-	sub $t2, $t2, $t7
-	sub $t3, $t3, $t7
-	
-	# Pop pixel address from stack
-	lw $t0, 0($sp)		# $t0 = address of pixel
-	addi, $sp, $sp, 4	# reclaim space on stack
-	
-	# Color the pixels appropriately
+sc_closed:
+	# Stamp a CLOSED clam
 	sw $t1, -16($t0)
 	sw $t1, -12($t0)
 	sw $t1, -8($t0)
@@ -667,25 +595,16 @@ stamp_closed_clam:
 	sw $t2, 4($t0)
 	sw $t2, 8($t0)
 	
+sc_exit:
 	# Return to caller
 	jr $ra
 # ---------------------------------------------------------------------------------------
 
 
-# stamp_piranha_L(*pixel):
-# 	"Stamps" a left-facing piranha onto the display given it is positioned at *pixel
-stamp_piranha_L:
-	li $t1, 0x00312e73	# $t1 = base color
-	li $t2, 0x00661a1f	# $t2 = belly color
-	li $t3, 0x009595ad	# $t3 = teeth color
-	li $t4, 0x00000000	# $t4 = black
-	jr $ra
-# ---------------------------------------------------------------------------------------
-
-
-# stamp_piranha_R(*pixel):
-# 	"Stamps" a right-facing piranha onto the display given it is positioned at *pixel
-stamp_piranha_R:
+# stamp_piranha(*pixel):
+# 	"Stamps" a piranha onto the display given it is positioned at *pixel
+#	$t0: pixel_address, $t1-$t4: colors, $t6: world, $t7: temp
+stamp_piranha:
 	li $t1, 0x00312e73	# $t1 = base color
 	li $t2, 0x00661a1f	# $t2 = belly color
 	li $t3, 0x009595ad	# $t3 = teeth color
@@ -696,7 +615,7 @@ stamp_piranha_R:
 
 # stamp_pufferfish(*pixel):
 # 	"Stamps" a pufferfish onto the display given it is positioned at *pixel
-#	Uses registers $t0-$t7
+#	$t0: pixel_address, $t1-$t5: colors, $t6: world, $t7: temp
 stamp_pufferfish:
 	li $t1, 0x00a8c267	# $t1 = base color
 	li $t2, 0x00929644	# $t2 = fin/spikes color
@@ -1063,7 +982,7 @@ stamp_pufferfish:
 	
 # stamp_seahorse(*pixel):
 # 	"Stamps" a seahorse onto the display given it is positioned at *pixel
-#	Uses registers $t0-$t3, $t6, $t7
+#	$t0: pixel_address, $t1-$t3: colors, $t6: world, $t7: temp
 stamp_seahorse:
 	li $t1, 0x00ff9815	# $t1 = seahorse colour
 	li $t2, 0x00ffeb3b	# $t2 = fin colour
@@ -1125,9 +1044,10 @@ stamp_seahorse:
 #########################################################################
 
 # get_bg_color():
-#	Helper function. Given the values in the struct `world`,
-#	Sets the register $a0 to the background color.
-#	Todo: see about what register I can use here
+#	Returns bg_color
+#	Given the values in the struct `world`, pushes the 
+#	correct background color onto the stack
+#	$t1: bg_color, $t3: world
 get_bg_color:
 	la $t3, world
 	lw $t3, ($t3)		# $t3 = world.level
@@ -1135,29 +1055,42 @@ get_bg_color:
 	beq $t3, 1, gbc_level_1	# branch if world.level == 1
 	beq $t3, 2, gbc_level_2	# branch if world.level == 2
 	beq $t3, 3, gbc_level_3	# branch if world.level == 3
-	li $a0, SEA_COL_4	# $a0 = bg color 4
+	li $t1, SEA_COL_4	# $t1 = bg color 4
 	j gbc_exit
 gbc_level_0:
-	li $a0, SEA_COL_0	# $a0 = bg color 0
+	li $t1, SEA_COL_0	# $t1 = bg color 0
 	j gbc_exit
 gbc_level_1:
-	li $a0, SEA_COL_1	# $a0 = bg color 1
+	li $t1, SEA_COL_1	# $t1 = bg color 1
 	j gbc_exit
 gbc_level_2:
-	li $a0, SEA_COL_2	# $a0 = bg color 2
+	li $t1, SEA_COL_2	# $t1 = bg color 2
 	j gbc_exit
 gbc_level_3:
-	li $a0, SEA_COL_3	# $a0 = bg color 3
+	li $t1, SEA_COL_3	# $t1 = bg color 3
 gbc_exit:
+	# Push result onto stack
+	addi $sp, $sp, -4
+	sw $t1, 0($sp)
 	jr $ra
 # ---------------------------------------------------------------------------------------
 
 
 # unstamp_crab():
 # 	Removes the crab from the display
-#	Uses registers $t0, $t1
+#	$t0: crab_address, $t1: bg_color
 unstamp_crab:
-	li $t1, SEA_COL_4	# $t1 = sea colour
+	# Push return address onto stack
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	# Obtain background color
+	jal get_bg_color
+	
+	# Pop result and old return address from stack
+	lw $t1, 0($sp)		# $t1 = sea colour
+	lw $ra, 4($sp)		# $ra = old return address
+	addi $sp, $sp, 8
 
 	# Get pixel address
 	la $t0, crab		# $t0 = addr(crab struct)
